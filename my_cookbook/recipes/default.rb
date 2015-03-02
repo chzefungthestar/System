394691b31d10d584fe3623da5a52f780715833fc
chzefungthestar <chzefungthestar@users.noreply.github.com>
2015-03-02 10:05:57 +0800

    Update default.rb

--- my_cookbook/recipes/default.rb
+++ my_cookbook/recipes/default.rb
@@ -1,9 +1,7 @@
-[ "/srv/www/dev_cm_lifestyle/current/wp-content/uploads" ].each do |path|
-	directory path do
-		mode 0775
-		owner 'deploy'
-		group 'www-data'
-		recursive true
-		action :create
-	end
+directory path do
+	mode 0775
+	owner 'deploy'
+	group 'www-data'
+	recursive true
+	action :create
 end
